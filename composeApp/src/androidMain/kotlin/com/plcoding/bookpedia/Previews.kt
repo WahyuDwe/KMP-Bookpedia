4d6ee3417c8ff989b974cd0ab7f2a8570e55fd9b
WahyuDwe <wdwihartarto@gmail.com>
2024-11-21 14:02:36 +0700

    finish setup list book

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -8,6 +8,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
+import com.plcoding.bookpedia.book.presentation.book_list.books
 import com.plcoding.bookpedia.book.presentation.book_list.component.BookSearchBar
 
 @Preview
@@ -26,4 +30,16 @@ private fun BookSearchBarPreview() {
                 .fillMaxWidth()
         )
     }
+}
+
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books,
+        ),
+        onAction = {}
+    )
 }
