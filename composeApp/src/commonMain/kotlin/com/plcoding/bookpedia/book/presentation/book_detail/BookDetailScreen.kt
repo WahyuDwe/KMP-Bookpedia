80e2dc02479fa2003c021f6fff9b1cc78f41c65a
WahyuDwe <wdwihartarto@gmail.com>
2024-11-22 18:38:15 +0700

    add detail screen

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -0,0 +1,34 @@
+package com.plcoding.bookpedia.book.presentation.book_detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.bookpedia.book.presentation.SelectedBookViewModel
+
+@Composable
+fun BookDetailScreenRoot(
+    viewModel: BookDetailViewModel,
+    onBackClick: () -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookDetailScreen(
+        state = state,
+        onAction = { action ->
+            when(action) {
+                is BookDetailAction.OnBackClicked -> onBackClick()
+                else -> Unit
+            }
+            viewModel.onAction(action)
+        }
+    )
+}
+
+@Composable
+private fun BookDetailScreen(
+    state: BookDetailState,
+    onAction: (BookDetailAction) -> Unit
+) {
+
+}
